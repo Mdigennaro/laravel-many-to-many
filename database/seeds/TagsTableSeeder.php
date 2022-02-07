0bea2488778dcd776c62014f4984999f8c839526
Mdigennaro <michele.digennaro719@gmail.com>
2022-02-07 15:43:39 +0100

    creata tabella tag

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Tag;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['Front end', 'Back end', 'Design', 'UX', 'Laravel', 'VueJs'];
+
+        foreach($tags as $tag){
+            $new_tag = new Tag();
+
+            $new_tag->name = $tag;
+            $new_tag->slug = Str::slug($new_tag->name, '-');
+            $new_tag->save();
+        }
+    }
+}
